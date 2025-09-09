b46d6b4aac0e6841fb56c4dd97915255a050a6cf
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-09-09 15:04:23 +0300

    Initial Commit

---
+++ Mobilist-iOS-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobilist-iOS-Challenge
+//
+//  Created by Beyza Nur Tekerek on 9.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
