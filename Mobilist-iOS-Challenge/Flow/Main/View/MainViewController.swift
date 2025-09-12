1f4a03f8a8262d961c7722c6423f0d51be8f7ced
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-09-12 16:04:40 +0300

    update

--- Mobilist-iOS-Challenge/Flow/Main/View/MainViewController.swift
+++ Mobilist-iOS-Challenge/Flow/Main/View/MainViewController.swift
@@ -9,10 +9,48 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let viewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "MovieCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "MovieCollectionViewCell")
+        
+        viewModel.fetchPopularMovies { [weak self] success in
+            if success {
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
+            }
+        }
         
     }
 
+}
+
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as? MovieCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let movie = viewModel.movies[indexPath.row]
+        cell.configure(with: movie)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width - 32
+        return CGSize(width: width, height: 140)
+    }
 }
