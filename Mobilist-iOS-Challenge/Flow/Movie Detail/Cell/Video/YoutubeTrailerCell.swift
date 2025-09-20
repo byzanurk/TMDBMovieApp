2fcbe831935d01bacfe9744f9df3c8a321d40823
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-09-20 18:20:49 +0300

    create youtube trailer cell and youtube model

---
+++ Mobilist-iOS-Challenge/Flow/Movie Detail/Cell/Video/YoutubeTrailerCell.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeTrailerCell.swift
+//  Mobilist-iOS-Challenge
+//
+//  Created by Beyza Nur Tekerek on 19.09.2025.
+//
+
+import UIKit
+
+class YoutubeTrailerCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Mobilist-iOS-Challenge/Flow/Movie Detail/Model/YoutubeModel.swift
@@ -0,0 +1,8 @@
+//
+//  YoutubeModel.swift
+//  Mobilist-iOS-Challenge
+//
+//  Created by Beyza Nur Tekerek on 20.09.2025.
+//
+
+import Foundation
